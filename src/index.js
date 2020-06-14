57a1b64cc8419c6faf2b92598e6ac37f215e682e
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-06-14 12:55:26 -0300

    added redux parameters

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./routes";
+import { Provider } from "react-redux";
+import Store from "./stores";
 import { ThemeProvider } from "styled-components";
 import { neomorphic, GlobalStyle } from "./components";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={neomorphic}>
-      <Routes />
-      <GlobalStyle />
-    </ThemeProvider>
+    <Provider store={Store}>
+      <ThemeProvider theme={neomorphic}>
+        <Routes />
+        <GlobalStyle />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
