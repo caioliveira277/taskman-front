0fb37c9ec0d675e21c2d4fce025788ff8e93c3c1
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-06-28 19:25:50 -0300

    added new colors theme

--- src/components/theme/index.js
+++ src/components/theme/index.js
@@ -7,6 +7,8 @@ const neomorphic = {
     "linear-gradient(93.83deg,#f16f61 0.35%,#eb9877 96.77%)",
   BoxShadow1: "-10px -10px 20px #FFFFFF",
   BoxShadow2: "10px 10px 20px #D1D9E6",
+  ColorVariant1: "#de7f5f",
+  ColorVariant2: "#ee8b6a",
 };
 const GlobalStyle = createGlobalStyle`
   * {
