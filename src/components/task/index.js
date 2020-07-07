96394bcdab787256bdc7af0210bf77d9a6f87895
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-07-07 20:37:37 -0300

    added new modal in panel/tasks

--- src/components/task/index.js
+++ src/components/task/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { MdCheck, MdMenu } from "react-icons/md";
+import { useDispatch } from "react-redux";
 import { TaskComponent, ButtonRounded } from "./styles";
 
 export default function Task({ title, children, datetime }) {
+  const modalDispatch = useDispatch();
+  const HandlerClickEditTask = (e) => {
+    e.preventDefault();
+    modalDispatch({ type: "OPEN_MODAL", target: "EDIT_TASK" });
+  };
+
   return (
     <TaskComponent>
       <div className="header">
@@ -16,7 +23,7 @@ export default function Task({ title, children, datetime }) {
       <hr />
       <p>{children}</p>
       <div className="footer">
-        <ButtonRounded>
+        <ButtonRounded onClick={HandlerClickEditTask}>
           <i className="menu">
             <MdMenu />
           </i>
--- src/pages/panel/styles.js
+++ src/pages/panel/styles.js
@@ -1,13 +1,41 @@
 import styled from "styled-components";
+export { TaskComponent } from "../../components/task/styles";
 
 export const GridTasks = styled.section`
   width: 100%;
   display: flex;
   flex-flow: wrap column;
-  max-height: 960px;
+  max-height: 980px;
   overflow: auto;
   & > div {
     margin: 20px;
   }
   scrollbar-width: thin;
+`;
+
+export const FormEditTask = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  .inputs > :last-child {
+    margin-top: 25px;
+  }
+  .preview {
+    hr.separator {
+      margin: 25px 0;
+    }
+    h3 {
+      font: normal 16px "Roboto", sans-serif;
+      margin-bottom: 15px;
+    }
+    small {
+      font: 300 12px "Roboto", sans-serif;
+    }
+  }
+  .footer {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    margin-top: 35px;
+  }
 `;
