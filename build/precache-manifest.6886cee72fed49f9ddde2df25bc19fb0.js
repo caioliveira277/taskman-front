8f6349d5b3af59d79143100b74fe960f2a7780a7
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-09-02 22:13:46 -0300

    subindo build do projeto

---
+++ build/precache-manifest.6886cee72fed49f9ddde2df25bc19fb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23faaab3594f23541ca92e691e2529c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68a47f22112e6e26791a",
+    "url": "/static/js/2.863df07b.chunk.js"
+  },
+  {
+    "revision": "94b1d62adecd1cb1e2ed4aa8eb935044",
+    "url": "/static/js/2.863df07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fecf4271efa7f88f3bd",
+    "url": "/static/js/main.b51bb1c2.chunk.js"
+  },
+  {
+    "revision": "0d04b49c365a432e3e3f",
+    "url": "/static/js/runtime-main.4416f612.js"
+  }
+]);
